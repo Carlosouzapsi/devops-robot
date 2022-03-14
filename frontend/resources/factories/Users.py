635f6d704123216ff67a4a1fe3cbcd511a45182e
Carlos Souza <carlossouza@brj826t0t9.lan>
2022-03-14 11:37:09 -0300

    ajustando o run.sh de frontend para rodar a regressão completa em headless

--- frontend/resources/factories/Users.py
+++ frontend/resources/factories/Users.py
@@ -86,7 +86,14 @@ def factory_user(target):
             'name': 'Peter',
             'lastname': 'Parker',
             'email': 'parker@oscorp.com',
-            'password': 'pwd123'        
+            'password': 'pwd123',    
+            'geek_profile': {
+                    'whatsapp': '21999991002',
+                    'desc': 'Faço instalação de distribuições Linux no seu computador PC X86 ou X64 com processadores Intel ou AMD. Pode ser Linux Debian, Ubuntu, Mint, Fedora etc...',
+                    'printer_repair': 'Não',
+                    'work': 'Remoto',
+                    'cost': '200'
+            } 
         }
     }
     return data[target]
